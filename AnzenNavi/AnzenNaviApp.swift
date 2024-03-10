d68e754d8e6f53772c2de9d9dc6b348dd3949b05
hiromu <hiromureve1522@icloud.com>
2024-03-10 10:55:50 +0900

    Initial Commit

---
+++ AnzenNavi/AnzenNaviApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnzenNaviApp.swift
+//  AnzenNavi
+//
+//  Created by 田中大夢 on 2024/03/10.
+//
+
+import SwiftUI
+
+@main
+struct AnzenNaviApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
