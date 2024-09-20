b0233cfeea66bfbf87fe7f711a524f46f82f100f
hiromu <hiromureve1522@icloud.com>
2024-09-21 11:45:34 +1200

    Initial Xcode project setup

---
+++ AnzenNavi/AnzenNavi/AnzenNaviApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnzenNaviApp.swift
+//  AnzenNavi
+//
+//  Created by 田中大夢 on 2024/09/20.
+//
+
+import SwiftUI
+
+@main
+struct AnzenNaviApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnzenNavi/AnzenNaviTests/AnzenNaviTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnzenNaviTests.swift
+//  AnzenNaviTests
+//
+//  Created by 田中大夢 on 2024/09/20.
+//
+
+import Testing
+@testable import AnzenNavi
+
+struct AnzenNaviTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
