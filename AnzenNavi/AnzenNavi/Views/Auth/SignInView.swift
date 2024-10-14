f524e7d0fbbca1b9b1f1ea05c671054e2a6cc789
hiromu <hiromureve1522@icloud.com>
2024-10-14 23:53:35 +1300

    現在地の追加

--- AnzenNavi/AnzenNavi/Views/Auth/SignInView.swift
+++ AnzenNavi/AnzenNavi/Views/Auth/SignInView.swift
@@ -31,7 +31,7 @@ struct SignInView: View {
                     Spacer()
                     headerText
                     appleSignInButton
-                    phoneNumberSignInButton // 既存の VStack 内に保持
+                    phoneNumberSignInButton
                 }
                 .padding(20)
             }
@@ -56,7 +56,7 @@ struct SignInView: View {
     
     private var backgroundImage: some View {
         GeometryReader { geometry in
-            Image("BG") // 画像名が "BG" であることを確認
+            Image("BG")
                 .resizable()
                 .aspectRatio(contentMode: .fill)
                 .frame(width: geometry.size.width, height: geometry.size.height)
--- AnzenNavi/AnzenNavi/Views/Map/MapView.swift
+++ AnzenNavi/AnzenNavi/Views/Map/MapView.swift
@@ -4,13 +4,60 @@
 //
 //  Created by 田中大夢 on 2024/09/22.
 //
-
 import SwiftUI
 import MapKit
 
 struct MapView: View {
+    let manager = CLLocationManager()
+    @State private var cameraPosition: MapCameraPosition = .userLocation(fallback: .automatic)
+    
     var body: some View {
-        Map()
+        ZStack {
+            mapView
+            userLocationButton
+        }
+        .onAppear {
+            manager.requestWhenInUseAuthorization()
+        }
     }
+    
+    // MARK: - View Components
+    
+    private var mapView: some View {
+        Map(position: $cameraPosition) {
+            UserAnnotation()
+        }
+    }
+    
+    private var userLocationButton: some View {
+        VStack {
+            Spacer()
+            HStack {
+                locationButton
+                    .padding(.leading)
+                Spacer()
+            }
+            .padding(.bottom, 150)
+        }
+    }
+    
+    private var locationButton: some View {
+        Button(action: {
+            if let currentCoordinate = manager.location?.coordinate {
+                cameraPosition = .userLocation(fallback: .region(MKCoordinateRegion(center: currentCoordinate, latitudinalMeters: 500, longitudinalMeters: 500)))
+            }
+        }) {
+            Image(systemName: "location.fill")
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.blue)
+                .clipShape(Circle())
+                .shadow(radius: 5)
+        }
+    }
+}
+
+#Preview {
+    MapView()
 }
 
